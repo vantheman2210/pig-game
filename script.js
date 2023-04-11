d63c4f91fdd9ff9a34ae3fa343e7a1f0621078de
vantheman2210 <martinovic.ba@gmail.com>
2023-04-11 12:51:11 +0200

    Switching player logic

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 'use strict';
 
 // Selecting elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -11,13 +15,26 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-
+const scores = [ 0, 0 ];
+let currentScore = 0;
+let activePlayer = 0;
 // Rolling dice functionality
-btnRoll.addEventListener('click', () => { 
-  // Generate random dice roll
-  const dice = Math.trunc(Math.random(0) * 6) + 1;
-  // Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `./images/dice-${dice}.png`
-  // Check for rolled 1, if true switch
-})
+btnRoll.addEventListener('click', () => {
+	// Generate random dice roll
+	const dice = Math.trunc(Math.random(0) * 6) + 1;
+	// Display dice
+	diceEl.classList.remove('hidden');
+	diceEl.src = `./images/dice-${dice}.png`;
+	// Check for rolled 1, if true switch
+	if (dice !== 1) {
+		currentScore += dice;
+		document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+	} else {
+		// switch to next player
+		document.getElementById(`current--${activePlayer}`).textContent = 0;
+		currentScore = 0;
+		activePlayer = activePlayer === 0 ? 1 : 0;
+		player0El.classList.toggle('player--active');
+		player1El.classList.toggle('player--active');
+	}
+});
